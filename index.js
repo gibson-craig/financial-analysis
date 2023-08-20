b9c879f861334c7fd23e273091f827f39ecd24c0
Craig G <gibson.craig05@gmail.com>
2023-08-20 19:03:06 +0100

    Added js calculating number of months present in the data

--- index.js
+++ index.js
@@ -94,4 +94,28 @@ var finances = [
     ['Jan-2017', 138230],
     ['Feb-2017', 671099],
   ];
-  
+
+
+  let months = finances.length;
+  let total = 0;
+  let change = 0;
+  let average;
+  let analysis;
+  let net = 0;
+  let netArray = [];
+  let netChangeSum = 0;
+// least min
+// greatest max
+// let least = []
+// let greatest = []
+
+for(let index = 0; index < finances.length; index++) {
+    for(let index2 = 0; index2 < finances[index].length; index2++)
+
+        if(typeof finances[index][index2] !== 'string') {
+            total += finances[index][index2];
+            change = finances[index][index2] - net;
+            net = finances[index][index2];
+            netArray.push(change);
+    }
+}
